bce7ae7e00030805a004697d9dd218b226816114
Neeraj <neeraj.tuteja+github@gmail.com>
2012-07-22 00:18:48 +0000

    All Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eepot::Application.config.secret_token = 'fb20045ce2cfca53233b1d24c4df50682422b74fee06223b68b54651922a8b56d0648eae78bb042038a2cf56f0e27da73ee66dfdd78475804d14fa2b5ef01b28'
